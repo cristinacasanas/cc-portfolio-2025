4cd15dd6617803e90b9aa2ac90a0d8b163ff96e5
Théo Daguier <92515854+theodaguier@users.noreply.github.com>
2025-05-25 19:33:42 +0200

    feat: 🎸 okay

--- src/components/layout/container.tsx
+++ src/components/layout/container.tsx
@@ -10,20 +10,13 @@ export const Container = ({
 	return (
 		<main
 			className={clsx(
-				"relative block grid-cols-4 gap-2 md:grid md:grid-cols-6 md:gap-4",
+				"relative block md:grid grid-cols-4 md:grid-cols-6 gap-2 md:gap-4",
 				"w-screen overflow-x-hidden overflow-y-hidden",
 				"pt-[var(--header-height)] md:pb-0",
 				"h-[100dvh] max-h-[100dvh]",
 				"px-1.5 lg:px-3",
 				className,
 			)}
-			style={
-				{
-					"--header-height": "42px",
-					"--mobile-thumbnails-bar-height": "50px",
-					"--filter-menu-bar-height": "85px",
-				} as React.CSSProperties
-			}
 		>
 			{children}
 		</main>
